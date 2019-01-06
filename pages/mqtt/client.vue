be21569febef9f50a94c634f45152ad8a61e7af3
이서연 <iseoyeon@iseoyeon-ui-MacBookPro.local>
2019-01-06 17:30:56 +0900

    init

---
+++ pages/mqtt/client.vue
@@ -0,0 +1,24 @@
+<template>
+    <section id="client">
+      <h1></h1>
+      <span>{{message}}}</span>
+    </section>
+</template>
+
+<script>
+  export default {
+      asyncData(context) {
+        return {message: "Hello ~ Mosquitto Client"}
+      }
+  }
+  const client = required("mqtt").connect('localhost:1883');
+
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(` :${message.toString()}`);
+    client.end()
+  })
+</script>
+
+<style scoped>
+</style>
---
+++ pages/mqtt/publisher.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <input type="text" name="message" placeholder="Please, input a text"  />
+    <button type="button">전송</button>
+  </div>
+</template>
+
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
